d8ecac9f6ad2155fbe23ecb3a5f6026cbee79fb1
Svyatoslav <svyatyar2@gmail.com>
2021-12-03 22:29:57 +0300

    add: IFeedVeiw, IMonitorFeedAdminView

--- Presentation/IMonitorFeedAdminView.cs
+++ Presentation/IMonitorFeedAdminView.cs
@@ -8,5 +8,11 @@ namespace Presentation
 {
     interface IMonitorFeedAdminView : IView
     {
+        string name { get; }
+        string typeOfFeeder { get; }
+
+        event Action ChoiseFeeder;
+        event Action ChoiseUser;
+
     }
 }
