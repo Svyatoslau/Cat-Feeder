c89224daa45a904aa144fd49d4c0f8ffb7ec0aaa
Svyatoslav <svyatyar2@gmail.com>
2021-12-03 16:17:30 +0300

    add: Customer

--- Model/Ententity/Customer.cs
+++ Model/Ententity/Customer.cs
@@ -6,6 +6,19 @@ namespace Model.Ententity
 {
     abstract class Customer
     {
+        private string name;
 
+        public string Name { get => name; set => name = value; }
+        public string Password { get => password; set => password = value; }
+        internal List<Timetable> Timetables { get => timetables; set => timetables = value; }
+        internal List<Log> Logs { get => logs; set => logs = value; }
+
+        private string password;
+
+        private List<Timetable> timetables;
+
+        private List<Log> logs;
+
+        
     }
 }
