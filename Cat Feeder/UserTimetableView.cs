f8752ca73330b2b8da6acc4a6a1ebb3e6c994b7c
Ivan <ivanchernetskey@mail.ru>
2021-12-04 02:08:08 +0300

    make user timetable view warning CHOOSE TIMETABLE

--- Cat Feeder/UserTimetableView.cs
+++ Cat Feeder/UserTimetableView.cs
@@ -56,72 +56,32 @@ namespace Cat_Feeder
 
         private void ExportTimetableButton_Click(object sender, EventArgs e)
         {
-
+            ExportTimetable.Invoke();
         }
 
         private void ImportTimetableButton_Click(object sender, EventArgs e)
         {
-
+            ImportTimetable.Invoke();
         }
 
         private void ChooseFeedButton_Click(object sender, EventArgs e)
         {
-
+            ChooseFeed.Invoke();
         }
 
         private void Timetable_Button1_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void Timetable_Button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Timetable_Button3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Timetable_Button4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Timetable_Button5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Timetable_Button6_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Timetable_Button7_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Timetable_Button8_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Timetable_Button9_Click(object sender, EventArgs e)
-        {
-
+            ChooseTimetable.Invoke();
         }
 
         private void SaveTametableButton_Click(object sender, EventArgs e)
         {
-
+            SaveTimetable.Invoke();
         }
 
         private void textBoxOfTimeTables_TextChanged(object sender, EventArgs e)
         {
-            textBoxOfTimeTables.Text;
+            
         }
     }
 }
