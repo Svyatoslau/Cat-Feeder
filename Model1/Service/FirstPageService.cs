58d858e3d23f1e2134c759e2e2d2bf8e60140307
Ivan <ivanchernetskey@mail.ru>
2021-12-17 21:48:22 +0300

    try to create login

--- Model1/Service/FirstPageService.cs
+++ Model1/Service/FirstPageService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model.Ententity;
+using DAL;
 
 namespace Model1.Service
 {
@@ -12,12 +14,22 @@ namespace Model1.Service
         
         public void createCustormer(string login, string password)
         {
+            UserRepository _repository = new UserRepository();
+            Customer PretenderCustomer = new Customer(login, password, "user");
+
             
+
+
+
             UserEvent?.Invoke();
         }
 
         public void createAdmin(string login, string password)
         {
+            Random random = new Random();
+            int ID = random.Next(0, 10000);
+
+            Customer customer = new Customer(ID, login, password, "admin");
 
             UserEvent?.Invoke();
         }
